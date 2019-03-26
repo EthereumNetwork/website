c650d5d69547d80ae4a56e44153b0691134c69b6
UnicornDev <unicorn.devmaster@gmail.com>
2019-03-26 21:17:26 +0200

    Add sidebar (v-navigation-drawer)
    https://github.com/disjfa/vuetify-sidebar-template

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+    <v-navigation-drawer v-model="drawer" app dark temporary>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              <span>Menu</span>
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-divider></v-divider>
+        <template v-for="(item, index) in items">
+          <v-list-tile :href="item.href" :to="{name: item.href}" :key="index">
+            <v-list-tile-action>
+              <v-icon dark v-html="item.icon"></v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title v-html="item.title"></v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar app dark>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title class="headline">
+        <span>Ethereum Network</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        icon
+        href="https://github.com/EthereumNetwork/website"
         target="_blank"
       >
-        <span class="mr-2">Latest Release</span>
         <v-icon>open_in_new</v-icon>
       </v-btn>
     </v-toolbar>
@@ -28,8 +50,24 @@ export default {
   name: 'App',
   data () {
     return {
-      //
-    }
+      drawer: false,
+      items: [{
+        href: 'home',
+        router: true,
+        title: 'Home',
+        icon: 'home',
+      }, {
+        href: 'about',
+        router: true,
+        title: 'About',
+        icon: 'domain',
+      }],
+    };
   }
 }
 </script>
+
+<style lang="stylus">
+  @import '../node_modules/vuetify/src/stylus/main';
+  @import 'css/main.css';
+</style>
