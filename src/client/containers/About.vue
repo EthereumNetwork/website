4351272d3a6ae649e2951199169075c15f9f3b96
Ritesh Choudhary <rkumar.iar@gmail.com>
2019-05-10 20:31:33 +0530

    Added startpage on about route.

--- src/client/containers/About.vue
+++ src/client/containers/About.vue
@@ -1,21 +1,31 @@
 <template>
-  <v-container>
-    <v-layout row wrap>
-      <v-flex xs12 lg12 mb-3>
-        <v-expansion-panel popout>
-          <v-expansion-panel-content
-            v-for="(item,i) in 5"
-            :key="i"
-          >
-            <template v-slot:header>
-              <div>Item</div>
-            </template>
-            <v-card>
-              <v-card-text>Ooooooh, woooooooooooooooowww its working!!!</v-card-text>
-            </v-card>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card v-for="(item,i) in 20"
+            :key="i">
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0"><a :id="i" :href="`#${i}`">Item {{i}}</a></h3>
+            <div> {{ card_text }} </div>
+          </div>
+        </v-card-title>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        card_text: 'Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat.'
+      }
+    }
+  }
+</script>
+
+<style>
+a {
+  text-decoration: none
+}
+</style>
