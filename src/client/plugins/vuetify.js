c7ed86c34ff3800274a7e0ac224baf44ed599927
Ritesh Choudhary <rkumar.iar@gmail.com>
2019-05-23 12:25:44 +0530

    set up tree shaking/a-la-carte

--- src/client/plugins/vuetify.js
+++ src/client/plugins/vuetify.js
@@ -1,7 +1,24 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
+import Vuetify, {
+  VApp, 
+  VNavigationDrawer,
+  VFooter,
+  VToolbar,
+  VFadeTransition
+} from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
+import { Ripple } from 'vuetify/lib/directives'
 
 Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VFooter,
+    VToolbar,
+    VFadeTransition
+  },
+  directives: {
+    Ripple
+  },
   iconfont: 'md'
 })
